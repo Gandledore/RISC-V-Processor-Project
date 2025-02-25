771ea5b0542cc3001c8d8ba0d60e4a8bbfc72a88
Erika Maquiling <erikacmaquiling@gmail.com>
2025-02-25 14:49:10 -0800

    Made UJ branch, still need to unscramble immediate fiel

--- decoder.c
+++ decoder.c
@@ -11,15 +11,29 @@ char* slice(char* input,int start,int end){
 }
 
 char** splitter(char* input){
-    char* opcode = slice(input,25,31);
-    printf("%s",opcode);
+    char* opcode = slice(input, 25, 31);
+    char* F1 = slice(input, 19, 24);
+    printf("field 1: %s\n", F1);
+
+    printf("opcode: %s\n",opcode);
+    
+    if (strcmp(opcode, "1101111") == 0) { // type UJ 
+        printf("type UJ\n");
+        char* F2 = slice(input, 0, 21);
+        printf("immediate: %s\n", F2); // TODO: have to unscramble immediate field 
+
+        char** test = 0;
+        return test; // need to break somehow?
+    }
+    
     char** test = 0;
     return test;
 }
 
 
 int main(){
-    char* inp = "00000000001100100000001010110011";
+    char* inp = "00000000101000000000000011101111";
+    printf("input: %s\n",inp);
     char** test = splitter(inp);
     return 0;
 }
