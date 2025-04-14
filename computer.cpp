fe9cfffe2f16295e396fc12af7ba40fd31537afb
Gandledore <rsmilstrey@gmail.com>
2025-04-14 14:20:09 -0700

    file reorganization

--- computer.cpp
+++ computer.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
-#include "processor.h"
+#include "classes/processor.h"
 
 using namespace std;
 
 int main(){
     Processor p(32,32);
-    if(p.load("test3.txt")){
+    if(p.load("test_cases/binary_tests/test3.txt")){
         p.run();
     }
     p.print();
